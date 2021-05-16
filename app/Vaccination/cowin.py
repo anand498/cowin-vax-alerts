ccf1765e8fb80b78cb726346ef9f2603a416c4c4
anand498 <anand.pandey803@gmail.com>
2021-05-16 15:49:17 +0530

    changing css and few run time configuration

--- app/Vaccination/cowin.py
+++ app/Vaccination/cowin.py
@@ -2,8 +2,8 @@ from fastapi import status
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from app.config.dbconfig import DB,client,COLLECTION_NAME
-
-
+import pandas as pd
+df_districts=pd.read_csv("app/static/DistrictListwithstates.csv")
 def jsonresponse(reasonCode,status,reasonText):
     json={
         "reasonCode":reasonCode,
@@ -18,14 +18,15 @@ def subscribeuser(userdetails):
     district=userdetails.district
     state=userdetails.state
     agegroup=userdetails.agegroup
+    districtid=df_districts.loc[df_districts['District_Name'] == district, 'District_ID'].iloc[0]
     if name=="" or email =="" or name is None or email is None:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content=jsonresponse('400','Error','Payload in Invalid'))
-
     record={
         "name": str(name),
         "email": str(email),
-        "district_name":str(district),
         "state_name":str(state),
+        "district_name":str(district),
+        "district_id":str(districtid),
         "age":str(agegroup)
     }
     user_collection = client[DB][COLLECTION_NAME]
@@ -40,6 +41,7 @@ def deleteuser(userdetails):
     myquery = {'email':email}
     records.delete_many(myquery)
     return JSONResponse(status_code=status.HTTP_200_OK,content=jsonresponse('200','Records Deleted'))
+
 
 
 
