afde02bb792597dfeffc5444e3a7cf7fcc57cf0a
anand498 <anand.pandey803@gmail.com>
2021-05-17 08:25:49 +0530

    adding ssl and changing content on how it works page

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from fastapi.staticfiles import StaticFiles
 from app.config import app_config 
 from app.route import route
 from pathlib import Path
-
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 app=FastAPI()
+
 app.mount(
     "/static",
     StaticFiles(directory= "app/static"),
     name="static",
 )
-
+app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["myvaccinetracker.in", "*.myvaccinetracker.in"]
+)
 app.include_router(route, tags=["Vaccine Tracker"])
