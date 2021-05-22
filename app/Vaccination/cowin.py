da5818bd7d1d08b76999e11e3e810eaced50ebd2
anand498 <anand.pandey803@gmail.com>
2021-05-22 08:45:16 +0530

    adding mail methods for subs and unsub of alert with requirements

--- app/Vaccination/cowin.py
+++ app/Vaccination/cowin.py
@@ -1,14 +1,26 @@
 from fastapi import status
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
-from app.config.dbconfig import DB,client,COLLECTION_NAME
 import pandas as pd
+import json,smtplib 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from jinja2 import Environment        
+from app.config import app_config
+from smtplib import SMTP
+from pymongo import MongoClient
+import ssl,os
+from pretty_html_table import build_table
+from icecream import ic
+from datetime import datetime
 df_districts=pd.read_csv("app/static/DistrictListwithstates.csv")
-def jsonresponse(reasonCode,status,reasonText):
+client = MongoClient(app_config.DB_URL,ssl_cert_reqs=ssl.CERT_NONE)
+
+
+def jsonresponse(reasonCode,status):
     json={
         "reasonCode":reasonCode,
-        "status":status,
-        "reasonText":reasonText
+        "status":status
     }
     return json
 
@@ -19,34 +31,111 @@ def subscribeuser(userdetails):
     state=userdetails.state
     agegroup=userdetails.agegroup
     districtid=df_districts.loc[df_districts['District_Name'] == district, 'District_ID'].iloc[0]
-    if name=="" or email =="" or name is None or email is None:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content=jsonresponse('400','Error','Payload in Invalid'))
+    if name=="" or email =="" or agegroup =="" or state =="" or name is None or email is None or agegroup is None or state is None :
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content=jsonresponse('400','Error - Payload in Invalid'))
     record={
         "name": str(name),
         "email": str(email),
         "state_name":str(state),
         "district_name":str(district),
         "district_id":str(districtid),
-        "age":str(agegroup)
+        "age":str(agegroup),
+        "createdDateTime":str(datetime.now().replace(microsecond=0))
     }
-    user_collection = client[DB][COLLECTION_NAME]
+    
+    user_collection = client[app_config.DB][app_config.COLLECTION_NAME]
     result = user_collection.insert_one(record)
     ack = result.acknowledged
     if (ack==True):
-        return JSONResponse(status_code=status.HTTP_200_OK,content=jsonresponse('200','User Subscribed','User has been registered'))
+        sendconfirmationmail(email,record)
+        return JSONResponse(status_code=status.HTTP_200_OK,content=jsonresponse('200','User Subscribed'))
+
+
+def sendconfirmationmail(receiver,record):
+    name=record['name']
+    receiver=record['email']
+    district_name=record['district_name']
+    record={
+        "Name": str(record['name']),
+        "Email": str(record['email']),
+        "State":str(record['state_name']),
+        "District":str(record['district_name']),
+        "Age Group":str(record['age'])
+    }
+    df = pd.json_normalize(record)
+    data=build_table(df,'green_dark',font_family = 'Century Gothic',text_align = 'center')
+    message = MIMEMultipart()
+    message['Subject'] = 'My Vaccine Tracker Registration'
+    message['From'] = 'myvaccinetracker@gmail.com'
+    message['To'] = receiver
+    VACCINE_TEMPLATE = """
+            <!DOCTYPE html>
+            <html>
+            <h4>Hi, {{name}}!</h4>
+            <p>Thanks for registering for Vaccination Alerts.</p>
+            <br>
+            <p>Here are the details captured by My Vaccine Tracker</p>
+            {{table}}
+            <br>
+            <p> To Book your Slot on Cowin, Click <a href="https://selfregistration.cowin.gov.in/">Here</a></p>
+            <p>Thanks,</p>
+            <p>My Vaccine Tracker Team</p>
+            <center> Developed by:<a href="https://www.linkedin.com/in/anand498/" target="_blank">anand498</a></center>
+            </html>
+            """  
+    message.attach(MIMEText(Environment().from_string(VACCINE_TEMPLATE).render(name=str(name),district_name=district_name,table=data), "html"))
+    msg_body = message.as_string()
+    
+    server = SMTP(app_config.EMAIL_HOST, app_config.EMAIL_PORT)
+    server.starttls()
+    server.login(app_config.EMAIL_HOST_USER, app_config.EMAIL_HOST_PASSWORD)
+    server.sendmail(message['From'], message['To'], msg_body)
+    server.quit()
 
 def deleteuser(userdetails):
     email=userdetails.email
-    records = client[DB][COLLECTION_NAME]
-    myquery = {'email':email}
-    records.delete_many(myquery)
-    return JSONResponse(status_code=status.HTTP_200_OK,content=jsonresponse('200','Records Deleted'))
+    db=app_config.DB
+    collectionname=app_config.COLLECTION_NAME
+    db = client[db]
+    coll = db[collectionname]
+    myquery = {"email":email}
+    if coll.find(myquery).count() > 0:
+        for x in coll.find(myquery):
+            name=x['name']
+            break
+        coll.delete_many(myquery)
+
+    else:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content=jsonresponse('400','User Does not Exist'))
+    sendconfirmationmaildelete(email,name)
+    return JSONResponse(status_code=status.HTTP_200_OK,content=jsonresponse('200','User Deleted'))
 
 
-
-
-
-
+def sendconfirmationmaildelete(receiver,name):
+    message = MIMEMultipart()
+    message['Subject'] = 'My Vaccine Tracker Unsubscription'
+    message['From'] = 'myvaccinetracker@gmail.com'
+    message['To'] = receiver
+    VACCINE_TEMPLATE = """
+            <!DOCTYPE html>
+            <html>
+            <h4>Hi, {{name}}!</h4>
+            <p>You have been unsubscribed from receiving any further updates. </p>
+            <p>If you would like to state the reason for your unsubscription please click <a href="https://forms.gle/FeDmaN9opKGzZAf3A">Here</a>
+            <br>
+            <p>Thanks,</p>
+            <p>My Vaccine Tracker Team</p>
+            <center> Developed by:<a href="https://www.linkedin.com/in/anand498/" target="_blank">anand498</a></center>
+            </html>
+            """  
+    message.attach(MIMEText(Environment().from_string(VACCINE_TEMPLATE).render(name=str(name)), "html"))
+    msg_body = message.as_string()
+    
+    server = SMTP(app_config.EMAIL_HOST, app_config.EMAIL_PORT)
+    server.starttls()
+    server.login(app_config.EMAIL_HOST_USER, app_config.EMAIL_HOST_PASSWORD)
+    server.sendmail(message['From'], message['To'], msg_body)
+    server.quit()
 
 
 
