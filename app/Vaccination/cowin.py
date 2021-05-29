23da580cd8c68a7b1c0c363e11777ceaf49f9068
anand498 <anand.pandey803@gmail.com>
2021-05-29 14:14:10 +0530

    removed credentials and logging info

--- app/Vaccination/cowin.py
+++ app/Vaccination/cowin.py
@@ -2,30 +2,17 @@ from fastapi import status
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 import pandas as pd
-import json,smtplib 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from jinja2 import Environment        
+import json,ssl,os,pytz
 from app.config import app_config
-from smtplib import SMTP
-from pymongo import MongoClient
-import ssl,os
-from pretty_html_table import build_table
-from icecream import ic
+from pymongo import MongoClient 
 from datetime import datetime
-import logging
 from time import sleep
-import pytz
 from pytz import timezone
-
-logger=logging.getLogger() 
-logger.setLevel(logging.INFO) 
 
 def timetz(*args):
     return datetime.now(tz).timetuple()
 
 tz = timezone('Asia/Kolkata') 
-logging.Formatter.converter = timetz
 df_districts=pd.read_csv("app/static/DistrictListwithstates.csv")
 client = MongoClient(app_config.DB_URL,ssl_cert_reqs=ssl.CERT_NONE)
 
@@ -37,6 +24,7 @@ def jsonresponse(reasonCode,status):
     }
     return json
 
+
 def subscribeuser(userdetails):
     name=userdetails.name
     email=userdetails.email
@@ -47,7 +35,6 @@ def subscribeuser(userdetails):
     collectionname=app_config.COLLECTION_NAME
     districtid=df_districts.loc[df_districts['District_Name'] == district, 'District_ID'].iloc[0]
     if name.isspace() == True or email.isspace()==True  or agegroup =="" or state =="" or name is None or email is None or agegroup is None or state is None :
-        logger.error("Invalid request")
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content=jsonresponse('400','Error - Payload in Invalid'))
     record={
         "name": str(name),
@@ -62,7 +49,6 @@ def subscribeuser(userdetails):
     coll = db[collectionname]
     myquery = {"email":email,"name":name,"district_name":district,"age":agegroup}
     if coll.find(myquery).count() > 0:
-        logger.error("Already Exists Name: "+str(name))
         return JSONResponse(status_code=status.HTTP_403_FORBIDDEN,content=jsonresponse('403','Entry against value already exists'))
     
     result = coll.insert_one(record)
@@ -70,53 +56,23 @@ def subscribeuser(userdetails):
     coll2=db[collection_name_bkp]
     result2=coll2.insert_one(record)
     ack = result.acknowledged
+    record={
+        "name": str(name),
+        "email": str(email),
+        "state_name":str(state),
+        "district_name":str(district),
+        "district_id":str(districtid),
+        "age":str(agegroup),
+        'confirmation_type':'register'
+    }
+    invoke_response = app_config.lambda_client.invoke(FunctionName='mail_sender_confirmation',
+                                    InvocationType='Event',
+                                    Payload=json.dumps(record))
     if (ack==True):
-        logger.info("Registration success for Beneficiary Name: "+name+" Email: "+email+" district_name: "+district+" age-group: "+agegroup)
         return JSONResponse(status_code=status.HTTP_201_CREATED,content=jsonresponse('201','User Subscribed'))
 
 
-def sendconfirmationmail(receiver,record):
-    name=record['name']
-    receiver=record['email']
-    district_name=record['district_name']
-    record={
-        "Name": str(record['name']),
-        "Email": str(record['email']),
-        "State":str(record['state_name']),
-        "District":str(record['district_name']),
-        "Age Group":str(record['age'])
-    }
-    df = pd.json_normalize(record)
-    data=build_table(df,'green_dark',font_family = 'Century Gothic',text_align = 'center')
-    message = MIMEMultipart()
-    message['Subject'] = 'My Vaccine Tracker Registration'
-    message['From'] = 'myvaccinetracker@gmail.com'
-    message['To'] = receiver
-    VACCINE_TEMPLATE = """
-            <!DOCTYPE html>
-            <html>
-            <h4>Hi, {{name}}!</h4>
-            <p>Thanks for registering for Vaccination Alerts.</p>
-            <br>
-            <p>Here are the details captured by My Vaccine Tracker</p>
-            {{table}}
-            <br>
-            <br>
-            <p> To Book your Slot on Cowin, Click <a href="https://selfregistration.cowin.gov.in/">Here</a></p>
-            <p>Thanks,</p>
-            <p>My Vaccine Tracker Team</p>
-            <center> Developed by:<a href="https://www.linkedin.com/in/anand498/" target="_blank">Anand Pandey</a></center>
-            </html>
-            """  
-    message.attach(MIMEText(Environment().from_string(VACCINE_TEMPLATE).render(name=str(name),district_name=district_name,table=data), "html"))
-    msg_body = message.as_string()
-    
-    server = SMTP(app_config.EMAIL_HOST, app_config.EMAIL_PORT)
-    server.starttls()
-    server.login(app_config.EMAIL_HOST_USER, app_config.EMAIL_HOST_PASSWORD)
-    server.sendmail(message['From'], message['To'], msg_body)
-    logger.info("Registration Confirmation Mail Sent to: "+name+" at Email: "+str(receiver))
-    server.quit()
+
 
 def deleteuser(userdetails):
     email=userdetails.email
@@ -133,37 +89,16 @@ def deleteuser(userdetails):
 
     else:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content=jsonresponse('400','User Does not Exist'))
-    sendconfirmationmaildelete(email,name)
-    logger.info("Beneficiary Deleted Name:"+str(name)+" Email: "+str(email))
+    record={
+        'email': str(email),
+        'name': str(name),
+        'confirmation_type':'delete'
+    }
+    invoke_response = app_config.lambda_client.invoke(FunctionName='mail_sender_confirmation',
+                                    InvocationType='Event',
+                                    Payload=json.dumps(record))
     return JSONResponse(status_code=status.HTTP_200_OK,content=jsonresponse('200','User Deleted'))
 
-
-def sendconfirmationmaildelete(receiver,name):
-    message = MIMEMultipart()
-    message['Subject'] = 'My Vaccine Tracker Unsubscription'
-    message['From'] = 'myvaccinetracker@gmail.com'
-    message['To'] = receiver
-    VACCINE_TEMPLATE = """
-            <!DOCTYPE html>
-            <html>
-            <h4>Hi, {{name}}!</h4>
-            <p>You have been unsubscribed from receiving any further updates. </p>
-            <p>If you would like to share the reason for your unsubscription, please click <a href="https://forms.gle/FeDmaN9opKGzZAf3A">Here</a>
-            <br>
-            <p>Thanks,</p>
-            <p>My Vaccine Tracker Team</p>
-            <center> Developed by:<a href="https://www.linkedin.com/in/anand498/" target="_blank">Anand Pandey</a></center>
-            </html>
-            """  
-    message.attach(MIMEText(Environment().from_string(VACCINE_TEMPLATE).render(name=str(name)), "html"))
-    msg_body = message.as_string()
-    
-    server = SMTP(app_config.EMAIL_HOST, app_config.EMAIL_PORT)
-    server.starttls()
-    server.login(app_config.EMAIL_HOST_USER, app_config.EMAIL_HOST_PASSWORD)
-    server.sendmail(message['From'], message['To'], msg_body)
-    logger.info("Deletion Confirmation Mail Sent to: "+name+" at Email: "+receiver)
-    server.quit()
 
 
 
--- app/config/app_config.py
+++ app/config/app_config.py
@@ -1,6 +1,6 @@
 import configparser
 import sys,os
-
+import boto3
 
 current_dir = os.path.realpath(__file__).split('/app_config.py')[0]
 config_parser = configparser.ConfigParser()
@@ -12,12 +12,9 @@ ENV=config_parser.get('APP','ENV')
 HOST=config_parser.get(ENV,'HOST')
 PORT=int(config_parser.get(ENV,'PORT'))
 DEBUGMODE=config_parser.get(ENV,'DEBUGMODE')
-EMAIL_HOST = config_parser.get(ENV,'EMAIL_HOST')
-EMAIL_HOST_USER = config_parser.get(ENV,'EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config_parser.get(ENV,'EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config_parser.get(ENV,'EMAIL_PORT')
 DB = config_parser.get(ENV,'DB')
 COLLECTION_NAME = config_parser.get(ENV,'COLLECTION_NAME')
 COLLECTION_NAME_BKP=config_parser.get(ENV,'COLLECTION_NAME_BKP')
 DB_URL = config_parser.get(ENV,'DB_URL')
-
+REGION=config_parser.get(ENV,'REGION')
+lambda_client = boto3.client('lambda',aws_access_key_id=config_parser.get(ENV,'ACCESS_KEY'),aws_secret_access_key=config_parser.get(ENV,'SECRET_KEY'),region_name=REGION)
