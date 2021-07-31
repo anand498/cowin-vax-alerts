7bfaea3f3b49f9613c97bbbc2b9abdb6e3dc7009
anand498 <anand.pandey803@gmail.com>
2021-07-31 19:53:03 +0530

    Update app_config.py

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -9,10 +9,6 @@ config_parser.read(default_config_file)
 
 ENV=config_parser.get('APP','ENV')
 
-DB = config_parser.get(ENV,'DB')
-COLLECTION_NAME = config_parser.get(ENV,'COLLECTION_NAME')
-COLLECTION_NAME_BKP=config_parser.get(ENV,'COLLECTION_NAME_BKP')
-DB_URL = config_parser.get(ENV,'DB_URL')
 REGION=config_parser.get(ENV,'REGION')
 dynamodb_resource=boto3.resource('dynamodb',aws_access_key_id=config_parser.get(ENV,'ACCESS_KEY'),aws_secret_access_key=config_parser.get(ENV,'SECRET_KEY'),region_name=REGION)
 lambda_client = boto3.client('lambda',aws_access_key_id=config_parser.get(ENV,'ACCESS_KEY'),aws_secret_access_key=config_parser.get(ENV,'SECRET_KEY'),region_name=REGION)
